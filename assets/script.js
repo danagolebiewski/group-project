feacbaafdf42c05a5eff591ee91c094fe71c5828
Nathan Aramyan <nathan.aramyan@gmail.com>
2022-08-01 18:00:01 -0600

    changed console log, testing push

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+console.log("Hello World v3.0 the best version ever")
+//variables
+
+//functions (pulling APIs filter out data putting into variables)
+
+//+/- logic (displaying data for restaurants and movies. Search by Zip Code/City, State. Filter by radius specifically with Zip Code search then filter by: cuisine (drop down menu), rating/reviews, or cost. Present data: Open-now) after restaurant selection, display movie theaters based on restaurant location
+
+//special functions (adding event listener: on search click and restaurant selection click)
